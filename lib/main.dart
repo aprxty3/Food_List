8a9d72a3e42ae0a5c1dc106761394b06ddfd0056
aprxrt3 <ajiprxty3@gmail.com>
2021-11-03 17:48:37 +0700

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:food_apps/ui/pages/profile_screen.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
     );
   }
 }
