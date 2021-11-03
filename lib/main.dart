811a9d6f6adc2802bad7cbe997c8f7139c02baba
aprxrt3 <ajiprxty3@gmail.com>
2021-11-03 17:27:20 +0700

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:lib';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
---
+++ lib/ui/pages/screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: const Color(0xFFE9E9E9),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+          )
+        ],
+      ),
+      body: Stack(children: <Widget>[
+        Positioned(
+          top: 0,
+          height: height * 0.35,
+          left: 0,
+          right: 0,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              bottom: const Radius.circular(40),
+            ),
+            child: Container(
+              color: Color.white,
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
