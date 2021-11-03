e72b3a786921565a5fe36b2439ee545089bc3759
aprxrt3 <ajiprxty3@gmail.com>
2021-11-03 17:28:52 +0700

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lib';
+import 'package:food/ui/pages/profile_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
