4ab007ed85be6726c13eaf20a866f12fac1bd5a6
aprxrt3 <ajiprxty3@gmail.com>
2021-11-05 18:10:46 +0700

    change stateful to stateless

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
