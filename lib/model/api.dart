494758837d5993999a60bdbbdb5e6e7c30324e4a
aprxrt3 <ajiprxty3@gmail.com>
2021-11-06 02:57:35 +0700

    get API

---
+++ lib/model/api.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:food_apps/model/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeAPI {
+//   var req = unirest("GET", "https://yummly2.p.rapidapi.com/feeds/list");
+
+// req.query({
+// 	"limit": "18",
+// 	"start": "0",
+// 	"tag": "list.recipe.popular"
+// });
+
+// req.headers({
+// 	"x-rapidapi-host": "yummly2.p.rapidapi.com",
+// 	"x-rapidapi-key": "c7e44a9fe9mshd6139e551f20b66p147763jsndce4d78c8c3f",
+// 	"useQueryString": true
+// });
+
+
+}
