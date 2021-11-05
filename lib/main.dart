426cc57ac35f6984e03f694be13da6fbff13cfd2
aprxrt3 <ajiprxty3@gmail.com>
2021-11-05 16:34:39 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:food_recipe/views/home.dart';
+import 'package:food_apps/view/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
-  @override 
-  Widget build(BuildContext context){
+class MyApp extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      tittle: 'Food Recipes',
+      title: 'Food Recipes',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.white,
         textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.white)
-        )
+          bodyText2: TextStyle(color: Colors.white),
+        ),
       ),
-    )
+      home: HomePage(),
+    );
   }
 }
